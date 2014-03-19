469eacd94e9a3125d64fd68a96349d45652e3839
Lajos Molnar <lajos@google.com>
2014-03-19 14:35:46 -0700

    stagefright: use media_codecs_google*.xml files
    Bug: 13549114
    Change-Id: I410e3b202e623f20d4e521935e14a4bc87ec2358

--- device-common.mk
+++ device-common.mk
@@ -71,6 +71,9 @@ PRODUCT_COPY_FILES += \
 
 # Media configuration
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/oppo/find7/media_codecs.xml:system/etc/media_codecs.xml \
     device/oppo/find7/media_profiles.xml:system/etc/media_profiles.xml \
     device/oppo/find7/mixer_paths.xml:system/etc/mixer_paths.xml
