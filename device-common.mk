346b9a66d79ad74842129fd8a86fd01e27568e21
Christopher N. Hesse <raymanfx@gmail.com>
2014-09-03 10:56:40 +0200

    Find7: enable overlay usage for display
    Change-Id: I452ce204cf0f22648f17a32b17d591b1e060241d

--- device-common.mk
+++ device-common.mk
@@ -157,7 +157,8 @@ PRODUCT_PACKAGES += \
     copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
-    memtrack.msm8974
+    memtrack.msm8974 \
+    liboverlay
 
 # OMX
 PRODUCT_PACKAGES += \
