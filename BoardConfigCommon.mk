af528c544a81b054f7b09275eef9d0c645af5e89
Christopher N. Hesse <yraymanfx@gmail.com>
2014-11-07 00:30:59 +0100

    enable EXTENDED_FONT_FOOTPRINT for Find7
    please enjoy your new selection of CJK fonts!
    
    Change-Id: Ic5bc24ac243919e2aeaa6a1581f5ca78aed275cc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -195,3 +195,6 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
